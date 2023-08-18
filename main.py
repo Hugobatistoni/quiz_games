6b44ece2dec732fea12002c2cd4609d557f6216f
Hugo Batistoni <66275889+Hugobatistoni@users.noreply.github.com>
2023-08-18 17:15:07 -0300

    Update main.py
    Adicionado a Estrutura de repetição
    While True

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pycountry
 from random import randint
 
+while True:
 #gera um pais aleatorio e print o resultado
-paises = list(pycountry.countries)
-indice_aleatorio = randint(0, len(paises) - 1)
-paisAleatorio = paises[indice_aleatorio].name
-print(f'O pais aleatório selecionado foi: {paisAleatorio}')
+  paises = list(pycountry.countries)
+  indice_aleatorio = randint(0, len(paises) - 1)
+  paisAleatorio = paises[indice_aleatorio].name
+  print(f'O pais aleatório selecionado foi: {paisAleatorio}')
 
 ### o que fiz de errado na linha abaixo ?
-pais = input('Qual é o pais')
-if pais == paisAleatorio :
+  pais = input('Qual é o pais')
+
+  if pais == paisAleatorio :
     print('Você acertou')
-else:
+    break
+  else:
     print('Você errou')
     print(f'O pais certo é: {paisAleatorio}')
