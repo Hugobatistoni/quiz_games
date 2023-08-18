a72e32bec0b7a788e23b9e966b63b1964a75f57c
Hugo Batistoni <66275889+Hugobatistoni@users.noreply.github.com>
2023-08-18 16:52:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import pycountry
 from random import randint
 
 #gera um pais aleatorio e print o resultado
-paisAleatorio = list(pycountry.countries)[randint(1, 249)].name
-    print(f'O pais aleatório selecionado foi: {paisAleatorio}')
+paises = list(pycountry.countries)
+indice_aleatorio = randint(0, len(paises) - 1)
+paisAleatorio = paises[indice_aleatorio].name
+print(f'O pais aleatório selecionado foi: {paisAleatorio}')
 
 ### o que fiz de errado na linha abaixo ?
 pais = input('Qual é o pais')
 if pais == paisAleatorio :
     print('Você acertou')
-else
+else:
     print('Você errou')
+    print(f'O pais certo é: {paisAleatorio}')
